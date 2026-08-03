ab1b7c9787503a5e40b99c5ba3c826b824f004f2
Frodo <ronmaecus@gmail.com>
2026-08-03 09:28:16 +0000

    loaded options/keymaps/autocmds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,163 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+--vim.keymap.set("n", "<leader>sl", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>fm", vim.cmd.Ex)
+--vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>M")
+vim.keymap.set("n", "<C-u>", "<C-u>M")
+-- vim.keymap.set("n", "<C-f>", "<C-f>M")
+-- vim.keymap.set("n", "<C-b>", "<C-b>M")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+-- vim.keymap.set("n", "n", "nzz")
+-- vim.keymap.set("n", "N", "Nzz")
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>x", [["_x]])
+vim.keymap.set({ "n", "v" }, "<leader>c", [["_c]])
+vim.keymap.set({ "n", "v" }, "<leader>D", [["_D]])
+vim.keymap.set({ "n", "v" }, "<leader>C", [["_C]])
+vim.keymap.set({ "n", "v" }, "<leader>x", [["_x]])
+
+-- This is going to get me cancelled
+-- vim.keymap.set("n", "<C-s>", vim.cmd.w)
+-- vim.keymap.set("i", "<C-c>", "<Esc>")
+-- vim.keymap.set("n", "<C-c>", [["+yy]])
+-- vim.keymap.set("v", "<C-c>", [["+y]])
+
+-- surround with Klammern
+vim.keymap.set("v", "(", "c()<Esc>Pl%")
+vim.keymap.set("v", "[", "c[]<Esc>Pl%")
+vim.keymap.set("v", "{", "c{}<Esc>Pl%")
+
+vim.keymap.set("n", "<CR>", "<nop>")
+vim.keymap.set("n", "Q", "<nop>")
+--vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>g", vim.lsp.buf.format)
+
+local showCWindow = false
+vim.keymap.set("n", "<leader>o", function ()
+	if showCWindow then
+		vim.cmd.cclose()
+		showCWindow = false
+	else
+		vim.cmd.copen()
+		vim.cmd.wincmd("w")
+		showCWindow = true
+	end
+end)
+vim.keymap.set("n", "<leader>O", function ()
+	vim.cmd.cclose()
+	showCWindow = false
+end)
+-- vim.keymap.set("n", "<leader>o", "<cmd>copen<CR><C-w><C-w>") -- open the error log
+-- vim.keymap.set("n", "<leader>O", "<cmd>cclose<CR>")          -- close the error log
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")             -- walk through \
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")             -- error log
+
+-- spell checking
+local spellIsOn = false
+vim.keymap.set("n", "<F6>", function ()
+	if spellIsOn then
+		vim.opt.spell = false
+		spellIsOn = false
+	else
+		vim.opt.spell = true
+		spellIsOn = true
+	end
+end)
+-- vim.keymap.set("n", "<leader>j", "]szz")
+-- vim.keymap.set("n", "<leader>k", "[szz")
+vim.keymap.set("n", "<leader>j", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>k", vim.diagnostic.goto_prev)
+
+vim.keymap.set("n", "<leader>si", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gcI<Left><Left><Left><Left>]])
+vim.keymap.set("n", "<leader>ss", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>sh", [[:s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>X", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>so", function()
+    vim.cmd("so")
+end)
+
+-- reset the highlight after searching
+vim.keymap.set("n", "<Esc>", vim.cmd.nohlsearch)
+
+-- open files the old way (if you konow the name) / run through buffers.
+-- vim.keymap.set("n", "<leader>bn", vim.cmd.bn)
+-- vim.keymap.set("n", "<leader>bp", vim.cmd.bp)
+-- vim.keymap.set("n", "<leader>bd", vim.cmd.bd)
+-- vim.keymap.set("n", "<leader>bb", ":e <C-r>=expand('%:h')<CR>/")
+vim.keymap.set("n", "<leader>e", ":e <C-r>=expand('%:h')<CR>/")
+vim.keymap.set("n", "<C-^>", "<C-~>")
+
+-- Expanding braces and newline inserting
+vim.keymap.set("i", "{<Cr>", "{<Cr>}<Esc>O")
+
+vim.keymap.set("n", "<leader>mm", "<cmd>make<CR>")
+vim.keymap.set("n", "<leader>mf", "<cmd>make %<CR>")
+--vim.keymap.set("n", "<C-b>", vim.cmd.make)
+-- vim.keymap.set("n", "<leader>m", "<cmd>!mvn compile<CR>")
+-- vim.keymap.set("n", "<leader>t", "<cmd>mvn test<CR>")
+vim.keymap.set("i", "<C-Q>", "<C-R>=expand('%:t')<CR>")
+
+-- intuitive delete word
+vim.keymap.set("i", "<C-backspace>", "<C-w>")
+vim.keymap.set("i", "<C-w>", "<C-g>u<C-w>")
+vim.keymap.set("i", "<C-u>", "<C-g>u<C-u>")
+
+-- split stuff
+--vim.keymap.set("n", "<C-w><C-s>", vim.cmd.vsplit)
+
+-- easier to configure neovim
+vim.keymap.set("n", "<leader>rpp", "<cmd>e ~/.config/nvim/lua/config/remap.lua<CR>")
+vim.keymap.set("n", "<leader>rpc", "<cmd>e ~/.config/nvim/lua/config/set.lua<CR>")
+vim.keymap.set("n", "<leader>rps", "<cmd>e ~/.config/nvim/lua/plugins/snippets<CR>")
+vim.keymap.set("n", "<leader>rpS", "<cmd>e ~/.config/nvim/lua/plugins/luasnip.lua<CR>")
+vim.keymap.set("n", "<leader>rr", "<cmd>e ~/notes.md<CR>")
+
+
+-- reload luasnip
+-- vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/dev/luasnippets/snippets.lua<CR>")
+vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/lua/plugins/luasnip.lua<CR>")
+
+-- For LaTeX TODO: make only usable in tex-Files
+-- vim.keymap.set("i", "__", "_{")
+
+-- Exit insert mode
+vim.keymap.set("i", "jj", "j")
+vim.keymap.set("i", "kk", "k")
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "kj", "<Esc>")
+
+-- vim.keymap.set("i", "<C-l>", "<right>")
+-- vim.keymap.set("i", "<C-h>", "<left>")
+-- vim.keymap.set("i", "<C-f>", "<right>")
+-- vim.keymap.set("i", "<C-b>", "<left>")
+-- vim.keymap.set("i", "<C-p>", "<up>")
+-- vim.keymap.set("i", "<C-n>", "<down>")
+--vim.keymap.set("n", "<C-CR>", "o<Esc>")
+--vim.keymap.set("i", "<C-CR>", "<Esc>o")
+
+
+-- open link in browser
+vim.keymap.set("n", "gx", [[:silent !open <C-r><C-a> && i3-msg workspace 1 <CR>]])
+
+
+
+-- paste in quotes:
+vim.keymap.set("n", "<leader>pd", [[a"<Esc>"+pa"<Esc>]])
+vim.keymap.set("n", "<leader>ps", [[a'<Esc>"+pa'<Esc>]])
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,61 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+vim.opt.smartindent = true
+vim.opt.cindent = false
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- vim.opt.undodir = "/tmp/" .. os.getenv("USER") .. "/vim/undodir"
+vim.opt.undofile = true
+vim.opt.signcolumn = "yes"
+vim.opt.autowrite = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 7
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+vim.opt.foldenable = true
+vim.opt.foldmethod = "indent"
+
+vim.opt.splitright = true
+
+vim.opt.mouse = "a"
+
+vim.opt.list = true
+vim.opt.listchars = {
+	tab = "» ",
+	trail = "°",
+	nbsp = "⊕",
+	lead = "·",
+}
+
+vim.opt.spelllang = "de"
+vim.opt.spell = false
+
+vim.opt.cpoptions =  "aABceFsJ"
+
+-- local ft = require("Comment.ft")
+-- ft.text = "#%s"
